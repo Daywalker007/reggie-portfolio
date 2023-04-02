68676a9b17578c0576c895a6870686ad1823c0ff
Daywalker007 <reggie266@gmail.com>
2023-04-02 16:01:31 -0500

    Fix theme mode functionality

--- src/components/atoms/SkillCard.jsx
+++ src/components/atoms/SkillCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import 'boxicons'
 
-export default function SkillCard({name, desc}) {
+export default function SkillCard({name, desc, fillColor = 'black'}) {
   return (
     <div className='skill-card'>
-        <box-icon type='logo' name={name}></box-icon>
+        <box-icon type='logo' name={name} style={{fill: fillColor}}></box-icon>
         <div>{desc}</div>
     </div>
   )
--- src/components/molecules/SkillShowcase.jsx
+++ src/components/molecules/SkillShowcase.jsx
@@ -3,8 +3,16 @@ import SkillCard from '../atoms/SkillCard'
 
 export default function SkillShowcase() {
   return (
-    <section>
-        <SkillCard name='react' desc="React.js" />
+    <section className='skill-showcase col-90'>
+        <h2 className='font-size-lg'>Skills</h2>
+        <div className='skill-container col-50 flex justify-between'>
+          <SkillCard name='react' desc="React" fillColor={'#61DBFB'} />
+          <SkillCard name='javascript' desc="Javascript" fillColor={'#F0DB4F'} />
+          <SkillCard name='html5' desc="HTML" fillColor={'orange'} />
+          <SkillCard name='visual-studio' desc="C#" fillColor={'cyan'} />
+          <SkillCard name='css3' desc="CSS" fillColor={'blue'} />
+          <SkillCard name='sass' desc="SASS" fillColor={'pink'} />
+        </div>
     </section>
   )
 }
